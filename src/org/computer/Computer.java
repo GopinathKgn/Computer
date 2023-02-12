de63d56a8b3dd07506723f117e5c624dd07d2bb2
GOpinathKgn <gopikaruppasamy@gmail.com>
2023-02-12 17:19:16 +0530

    Update

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -1,7 +1,8 @@
 package org.computer;
 
 public class Computer {
-public void computerModel() {
-	System.out.println("Computer Model Name : Lenovo");
-}
+	public void computerModel() {
+		System.out.println("Computer Model Name : Lenovo");
+		System.out.println("System RAM : 2GB");
+	}
 }
