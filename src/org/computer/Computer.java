8c53f3274320a9136c5cd51a93fc4474cf87b2a7
GopinathKgn <124070468+GopinathKgn@users.noreply.github.com>
2023-02-12 18:14:34 +0530

    Final

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -6,7 +6,8 @@ public class Computer {
 		System.out.println("Computer RAM: 2B");
 		System.out.println("ROM size: 250GB");
 		System.out.println("System RAM : 2GB");
-		System.out.println("OS : Windows 7");
+		System.out.println("OS : Windows 7");em
+		System
 
 		// <<<<<<< HEAD
 		// =======
