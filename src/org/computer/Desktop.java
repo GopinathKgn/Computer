6c140f9f6fd8ccd64b246eb1246448acfa0f98c2
GopinathKgn <124070468+GopinathKgn@users.noreply.github.com>
2023-02-12 18:17:32 +0530

    Final

--- src/org/computer/Desktop.java
+++ src/org/computer/Desktop.java
@@ -3,6 +3,7 @@ package org.computer;
 public class Desktop extends Computer{
 	private void dekSize() {
 		System.out.println("Desktop Size : 1920*1080");
+		System.out.println();
 	}
 	public static void main(String[] args) {
 		Desktop d = new Desktop();
