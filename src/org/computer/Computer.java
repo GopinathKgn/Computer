3801e3cbdf6c2fb4aa3412bade53d1ddd19dce31
GOpinathKgn <gopikaruppasamy@gmail.com>
2023-02-12 17:38:30 +0530

    Upadate ROM

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -1,7 +1,10 @@
 package org.computer;
 
 public class Computer {
-public void computerModel() {
-	System.out.println("Computer Model Name : Lenovo");
-}
+	public void computerModel() {
+		System.out.println("Computer Model Name : Lenovo");
+		System.out.println("Computer RAM: 2B");
+		System.out.println("ROM size: 250GB");
+
+	}
 }
